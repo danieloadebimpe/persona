686ac534cc367d46b4509ba465a673d89e2eb17b
Marrmee <mhuberts2008@hotmail.com>
2023-05-20 21:43:57 +0200

    Add URI functions

--- contracts/contracts/Sbt.sol
+++ contracts/contracts/Sbt.sol
@@ -8,9 +8,25 @@ contract SoulBoundToken is ERC721 {
     error RegularERC721TransferFromAreNotAllowed();
     error RegularERC721SafeTransferFromAreNotAllowed();
 
-    constructor() ERC721("soul-bound token", "SBT") {
-        
+    string public uri;
+
+    constructor(string memory uri_) ERC721("soul-bound token", "SBT") {
+        uri = uri_;
     }
+
+    function setURI(string memory _uri) public {
+        uri = _uri;
+    }
+
+    /**
+     * @dev Base URI for computing {tokenURI}. If set, the resulting URI for each
+     * token will be the concatenation of the `baseURI` and the `tokenId`. Empty
+     * by default, can be overridden in child contracts.
+     */
+    function _baseURI() internal view virtual returns (string memory) {
+        return uri;
+    }
+
 
     function transferFrom(address, address, uint256) public virtual override {
         revert RegularERC721TransferFromAreNotAllowed();
