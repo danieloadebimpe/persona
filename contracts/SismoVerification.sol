cc312b45c44421a48727d3f06fc79f73d4c7d087
0xheartcode <0xheartcode@gmail.com>
2023-05-21 08:23:44 +0200

    Σ

--- contracts/SismoVerification.sol
+++ contracts/SismoVerification.sol
@@ -6,7 +6,7 @@ import "sismo-connect-solidity/SismoLib.sol";
 
 contract SismoVerification is ERC721, SismoConnect {
 
-    bytes16 public constant APP_ID = 0x149e519de68a4e8a93b7d41484f388cc; 
+    bytes16 public constant APP_ID = 0xaf51561270d2c8a362392d1a51ef8e18; 
     
     constructor() ERC721("Dummy", "NFT") SismoConnect(APP_ID)  
     {}
@@ -14,11 +14,21 @@ contract SismoVerification is ERC721, SismoConnect {
     function verifySismoConnect(bytes memory response) public {
         SismoConnectVerifiedResult memory result = verify ({
             responseBytes: response,
+            // we want to prove that they own a github repo
             auth: buildAuth({authType: AuthType.GITHUB}),
-            claim: buildClaim({groupId: 0x42c768bb8ae79e4c5c05d3b51a4ec74a }),
+
+            claim: buildClaim({groupId: 0x7d64fdd66ef50f491f675bf3d281f177}),
+
+            // // we also want to check if the signed message provided in the response is the signature of the user's address
             signature: buildSignature({message:abi.encode(msg.sender)})
         });
+
+
         uint256 tokenId = SismoConnectHelper.getUserId(result, AuthType.GITHUB);
         _mint(msg.sender, tokenId);
     }
+
+    // create a request object
+
+    // callback ?
 }
